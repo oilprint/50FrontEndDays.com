86e54ed667e5f8ae6eaee292facd6473b4f8b6fb
oilprint <irynakozlova.k@gmail.com>
2023-11-02 16:03:07 +0200

    finished day2

--- app/js/main.js
+++ app/js/main.js
@@ -14,3 +14,55 @@ cards.forEach(item => {
     item.classList.add('active');
   });
 })
+
+
+// day2. Creating The Steps
+
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const items = document.querySelectorAll('.day2__item');
+
+let currentActive = 1;
+
+console.log(next);
+next.addEventListener('click', () => {
+  currentActive++;
+  if (currentActive > items.length) {
+    currentActive = items.length;
+  }; 
+
+  update(); 
+})
+
+prev.addEventListener('click', () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  };
+
+  update(); 
+});
+
+function update(){
+  items.forEach((item, idx) => {
+    if (idx < currentActive) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  })
+
+  if (currentActive === 1) {
+  prev.disabled = true;
+  console.log("prev");
+} else if (currentActive === items.length) {
+  next.disabled = true;
+  console.log("next");
+} else {
+  prev.disabled = false;
+  next.disabled = false;
+}
+console.log(currentActive);
+}
+
